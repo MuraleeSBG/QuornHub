9c75f617cd6586933be29f0f3c2a0b367e96675f
〽️ <muralee.tumber@skybettingandgaming.com>
2023-09-26 09:15:43 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
