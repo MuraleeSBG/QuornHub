46fc1245d96b1b40e5126f01f977e528473f846f
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-05 09:11:58 +0100

    Create footer unfinished

---
+++ frontend/src/components/Footer/Footer.stories.jsx
@@ -0,0 +1,16 @@
+import { Footer } from './Footer';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Footer',
+  component: Footer,
+  tags: ['autodocs'],
+  
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+
+};
+
+
