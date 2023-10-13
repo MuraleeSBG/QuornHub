8ed9e7ae87dffa956407279b6d364ce9b7a69b18
〽️ <muralee.tumber@skybettingandgaming.com>
2023-10-13 10:24:31 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,29 +13,92 @@ app.get('/', (req, res) => {
 });
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "rootpass"
-  });
+  host: "localhost",
+  user: "root",
+  password: "rootpass"
+});
   
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
-  app.get('/api', (req, res) => {
-    con.query("SELECT * FROM QuornhubDb.recipes", function (err, result, fields) {
-        if (err) throw err;
-        res.send(result);
-        console.log("all works")
-      });
-  });
-
-
-  app.get('/api/:id', (req, res) => {
-    con.query("SELECT * FROM QuornhubDb.recipes WHERE id = ?", [req.params.id], function (err, result, fields) {
-        if (err) throw err;
-        res.send(result);
-        console.log("id works")
+app.get('/api', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("all works")
     });
 });
+
+
+app.get('/api/:id', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE id = ?", [req.params.id], function (err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+    console.log("id works")
+  });
+});
+
+app.get('/vegan', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE isVegan = 1", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("vegan works")
+    });
+});
+
+app.get('/gluten', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE isGlutenFree = 1", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("gluten works")
+    });
+});
+
+app.get('/nut', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE isNutFree = 1", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("nut works")
+    });
+});
+
+app.get('/lactose', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE isLactoseFree = 1", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("lactose works")
+    });
+});
+
+app.get('/u15', (req, res) => {
+  con.query("SELECT * FROM QuornhubDb.recipes WHERE isUnder15 = 1", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("u15 works")
+    });
+});
+
+app.delete('/api/:id', (req, res) => {
+  if (req.params.id) {
+    con.query("DELETE FROM QuornhubDb.recipes WHERE id = ?", [req.params.id], function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("delete works")
+    });
+  }
+});
+
+/*
+app.post('/createrecipe', (req, res) => {
+  console.log(req.body);
+  if (req.body) {
+    con.query("INSERT INTO QuornhubDb.recipes (recipeName, recipeImg, recipeDesc, isVegan, isGlutenFree, isNutFree, isLactoseFree, isUnder15, ingredients, method) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [req.body.recipeName, req.body.recipeImg, req.body.recipeDesc, req.body.isVegan, req.body.isGlutenFree, req.body.isNutFree, req.body.isLactoseFree, req.body.isUnder15, req.body.ingredients, req.body.method], function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log("post works")
+    });
+  }
+});
+*/
