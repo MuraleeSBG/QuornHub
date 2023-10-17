f37ade6daa25ea8cc2484b35eb0482606fe0abb3
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-17 18:58:35 +0100

    fixed bug with header resizing

--- frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
+++ frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
@@ -5,12 +5,14 @@ export const FoodSaverHeader = () => {
 	return (
 		<div className="food-saver-header">
             <Header/>
-            <h1 className="fs-header-title">A curated recipe list to help you reduce food waste</h1>
-            <p className="header-sub-heading">Simply submit each ingredient you have left over and our smart saver will find suitable recipes so you never have to throw away soggy vegetables again!</p>
-            <div className="search-bar">
-				<input className="search-input" type="text" placeholder="Type in an ingredient and press enter..." />
-			</div>
-
+            <div className="main">
+                <h1 className="fs-header-title">A curated recipe list to help you reduce food waste</h1>
+                <p className="header-sub-heading">Simply submit each ingredient you have left over and our smart saver will find suitable recipes so you never have to throw away soggy vegetables again!</p>
+                <div className="search-bar">
+                    <input className="search-input" type="text" placeholder="Type in an ingredient and press enter..." />
+                </div>
+            </div>
+        
 		</div>
 	);
 };
