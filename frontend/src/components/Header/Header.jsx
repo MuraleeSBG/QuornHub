c5b7d069b15d51a4bc0faa28c9647b1de2f6ecb1
Alice Smith <alice.smith@skybettingandgaming.com>
2023-10-17 10:52:44 +0100

    alice - add recipes and all recipes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,52 +2,48 @@ import "./Header.scss";
 import logo from "../../images/QHLogo.png";
 
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = ({ userName }) => {
-
-    const dropIcon = <FontAwesomeIcon icon={faCaretDown} />
+	const dropIcon = <FontAwesomeIcon icon={faCaretDown} />;
 
 	return (
-        <div className="header">
-            <div className="nav-bar">
+		<div className="header">
+			<div className="nav-bar">
+				<div>
+					<img className="QHlogo" src={logo} alt="Quornhub logo" />
+				</div>
 
-                <div>
-                    <img className="QHlogo" src={logo} alt="Quornhub logo"/>
-                </div>
-
-                <div className="nav-links">
-                    <Link className="nav" to="/">
+				<div className="nav-links">
+					<Link className="nav" to="/">
 						Home
-				    </Link>
-                    <Link className="nav" to="/food-saver">
+					</Link>
+					<Link className="nav" to="/food-saver">
 						Food Saver
 					</Link>
-                    <div className="dropdown nav">
-                        <button className="dropbtn2">Recipes{dropIcon}</button>
-                        <div className="dropdown-content2">
-                            <a href="gf">Gluten Free</a>
-                            <a href="nut">Vegan</a>
-                            <a href="lactofree">15 Minute</a>
+
+					<div className="dropdown nav">
+						<button className="dropbtn2">Recipes{dropIcon}</button>
+						<div className="dropdown-content2">
+							<a href="gf">Gluten Free</a>
+							<a href="nut">Vegan</a>
+							<a href="lactofree">15 Minute</a>
                             <a href="lactofree">Lactose Free</a>
-                            <a href="lactofree">Add Recipe</a>
-                            <a href="lactofree">All Recipes</a>
+                            <Link to='/add-recipes'>Add a Recipe</Link>
+							<Link to="/recipes">All Recipes</Link>
+						</div>
+					</div>
+				</div>
 
-                        </div>
-                    </div> 
-                    
-                </div>
-
-                    <div className="dropdown">
-                        <button className="dropbtn"></button>
-                        <div className="dropdown-content">
-                            <a href="login">Login</a>
-                            <a href="register">Register</a>
-                        </div>
-                    </div>
-            </div>
-        </div>
+				<div className="dropdown">
+					<button className="dropbtn"></button>
+					<div className="dropdown-content">
+						<a href="login">Login</a>
+						<a href="register">Register</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
