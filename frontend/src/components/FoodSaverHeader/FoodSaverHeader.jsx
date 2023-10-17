8eb36e504a8604018fbfb6cd794bb93af29f15cb
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-17 17:30:23 +0100

    Added search bar incomplete position for now

--- frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
+++ frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
@@ -6,7 +6,11 @@ export const FoodSaverHeader = () => {
 		<div className="food-saver-header">
             <Header/>
             <h1 className="fs-header-title">A curated recipe list to help you reduce food waste</h1>
-            <p className="header-sub-heading">Simply type in what ingredients you have left over and our smart saver will find suitable recipes so you never have to throw away soggy vegetables again!</p>
+            <p className="header-sub-heading">Simply submit each ingredient you have left over and our smart saver will find suitable recipes so you never have to throw away soggy vegetables again!</p>
+            <div className="search-bar">
+				<input className="search-input" type="text" placeholder="Enter food item..." />
+				<button className="submit-button">Submit</button>
+			</div>
 
 		</div>
 	);
