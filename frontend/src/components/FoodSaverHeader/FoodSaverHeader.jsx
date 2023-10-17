0cf1a4bd957c43f655a4f381885e157cd5b6a9b2
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-17 18:30:40 +0100

    search bar css

--- frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
+++ frontend/src/components/FoodSaverHeader/FoodSaverHeader.jsx
@@ -8,8 +8,7 @@ export const FoodSaverHeader = () => {
             <h1 className="fs-header-title">A curated recipe list to help you reduce food waste</h1>
             <p className="header-sub-heading">Simply submit each ingredient you have left over and our smart saver will find suitable recipes so you never have to throw away soggy vegetables again!</p>
             <div className="search-bar">
-				<input className="search-input" type="text" placeholder="Enter food item..." />
-				<button className="submit-button">Submit</button>
+				<input className="search-input" type="text" placeholder="Type in an ingredient and press enter..." />
 			</div>
 
 		</div>
