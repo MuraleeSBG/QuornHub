ee982eebc6f0c5c4aef908d8029be0df143ba934
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-24 14:39:51 +0100

    test data and json loader

--- frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
+++ frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
@@ -3,7 +3,7 @@ import "./FoodSaverCard.scss";
 export const FoodSaverCard = ({ image, title, tags }) => {
 	return (
 		<div className="card">
-			<img className="image" src={image} alt={title} />
+			<img className="recipe-image" src={image} alt={title} />
 			<div className="card-body">
 				<h3 className="card-title">{title}</h3>
 				<div className="tag-container">
