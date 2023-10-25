5205ab029e4a4bcadeca4c5bd15ae146e2b13f4a
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-10-25 16:15:47 +0100

    multiple input search functionality using test data - duplicate recipes and removing tags in the same session needs fixing

--- frontend/src/pages/FoodSaver/FoodSaver.jsx
+++ frontend/src/pages/FoodSaver/FoodSaver.jsx
@@ -8,15 +8,54 @@ import './FoodSaver.scss'
 const FoodSaver = () => {
 
 	const[listOfInput, setListOfInput] = useState([]);
+    let data = require('../../testData.json')
+    const [filteredData, setFilteredData] = useState([]);
+
+    //const[allRecipes, setAllRecipes] = useState([])
+    
 
     let addIngredient = (e) => {
 
         let ingredientToAdd = e.target.previousSibling.value
+
         setListOfInput((currentList) => [
             ...currentList,
             ingredientToAdd,
         ]);
+
+        console.log(`Ingredient to add is ${ingredientToAdd}` )
+        
         e.target.previousSibling.value = ''
+        filterData(ingredientToAdd)
+
+    }
+
+    const filterData = (ingredientToAdd) => {
+        const filteredData = data.filter((item) =>
+            item.ingredients.toLowerCase().includes(ingredientToAdd.toLowerCase())
+        )
+        console.log("The filteredData is")
+        console.log(filteredData)
+        // setFilteredData(filteredData)
+
+        for (let i=0; i < filteredData.length; i++) {
+            setFilteredData((currentList) => [
+                ...currentList,
+                filteredData[i],
+
+            ])
+
+        }
+        // console.log("updated filtered data")
+        // console.log(filteredData)
+
+        // const ids = filteredData.map(({id}) => id);
+        // const filtered = filteredData.filter(({id}, index) =>
+        // !ids.includes(id, index + 1));
+        // console.log("With duplicates removed")
+        // console.log(filtered);
+
+        // setFilteredData(filtered)
 
     }
 
@@ -42,8 +81,11 @@ const FoodSaver = () => {
                 </div>
     })
 
+    const fakeTags=['vegan', 'vegetarian', 'gluten-free']
 
-
+    const showResults = filteredData.map(recipe => {
+        return <FoodSaverCard title={recipe.recipeName} image={recipe.recipeImg} tags={fakeTags}/>
+    })
     
 
 
@@ -60,7 +102,7 @@ const FoodSaver = () => {
                 </div>
                 <div className="results-container">
                 {listOfInput.length !== 0
-                ? <FoodSaverCard image='https://handletheheat.com/wp-content/uploads/2020/10/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9-637x637-1-550x550.jpg' title='Vegan Chocolate Chip Cookies' tags={['vegan', 'vegetarian', 'gluten-free']}/>
+                ? <div>{showResults}</div>
                 : <h1 className="results-placeholder">Enter ingredients to start saving food and be inspired!</h1>
                 }
                 </div>
@@ -76,3 +118,5 @@ const FoodSaver = () => {
 };
 
 export default FoodSaver;
+
+{/* <FoodSaverCard image='https://handletheheat.com/wp-content/uploads/2020/10/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9-637x637-1-550x550.jpg' title='Vegan Chocolate Chip Cookies' tags={['vegan', 'vegetarian', 'gluten-free']}/> */}
