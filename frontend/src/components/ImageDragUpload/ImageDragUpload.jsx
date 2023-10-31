2b14578b26c96174bfe0069afd514b51609b87ba
Alice Smith <alice.smith@skybettingandgaming.com>
2023-10-31 09:55:52 +0000

    functionality done just no styling

--- frontend/src/components/ImageDragUpload/ImageDragUpload.jsx
+++ frontend/src/components/ImageDragUpload/ImageDragUpload.jsx
@@ -1,19 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./ImageDragUpload.scss";
+import upload from "../../images/upload-big-arrow.svg";
 
-// drag drop file component
-const ImageDragUpload = () => {
+const ImageDragUpload = ({ selectedImage, setSelectedImage }) => {
 	// drag state
 	const [dragActive, setDragActive] = useState(false);
-	// selected image state
-	const [selectedImage, setSelectedImage] = useState();
-	// ref
-	const inputRef = useRef(null);
-
-	useEffect(() => {
-		console.log(selectedImage);
-	}, [selectedImage]);
-
 	// handle drag events
 	const handleDrag = function (e) {
 		e.preventDefault();
@@ -25,8 +16,7 @@ const ImageDragUpload = () => {
 		}
 	};
 
-	// triggers when file is dropped
-	const handleDrop = function (e) {
+	const handleDrop = (e) => {
 		e.preventDefault();
 		e.stopPropagation();
 		setDragActive(false);
@@ -35,48 +25,47 @@ const ImageDragUpload = () => {
 		}
 	};
 
-	// triggers when file is selected with click
-	const handleChange = function (e) {
+	const handleChange = (e) => {
 		e.preventDefault();
 		if (e.target.files && e.target.files[0]) {
-			// handleFiles(e.target.files);
+			setSelectedImage(e.target.files[0]);
 		}
 	};
 
-	// triggers the input when the button is clicked
-	const onButtonClick = () => {
-		inputRef.current.click();
-	};
-
 	return (
-		<div id="form-file-upload" onDragEnter={handleDrag}>
-			<input
-				ref={inputRef}
-				type="file"
-				id="input-file-upload"
-				multiple={true}
-				onChange={handleChange}
-			/>
-			<label
-				id="label-file-upload"
-				htmlFor="input-file-upload"
-				className={dragActive ? "drag-active" : ""}
-			>
-				<div>
-					<p>Drag and drop your file here or</p>
-					<button className="upload-button" onClick={onButtonClick}>
-						Upload a file
-					</button>
+		<div>
+			{selectedImage ? (
+				<img id="form-file-upload" src={URL.createObjectURL(selectedImage)} />
+			) : (
+				<div id="form-file-upload" onDragEnter={handleDrag}>
+					<input
+						type="file"
+						id="input-file-upload"
+						multiple={true}
+						onChange={handleChange}
+					/>
+
+					<label
+						id="label-file-upload"
+						htmlFor="input-file-upload"
+						className={dragActive ? "drag-active" : ""}
+					>
+							<div>
+							<img className="uploadImage" src={upload} alt="upload image" />
+							<p>Drag and drop your file here or</p>
+							<p>click to upload</p>
+						</div>
+					</label>
+					{dragActive && (
+						<div
+							id="drag-file-element"
+							onDragEnter={handleDrag}
+							onDragLeave={handleDrag}
+							onDragOver={handleDrag}
+							onDrop={handleDrop}
+						></div>
+					)}
 				</div>
-			</label>
-			{dragActive && (
-				<div
-					id="drag-file-element"
-					onDragEnter={handleDrag}
-					onDragLeave={handleDrag}
-					onDragOver={handleDrag}
-					onDrop={handleDrop}
-				></div>
 			)}
 		</div>
 	);
