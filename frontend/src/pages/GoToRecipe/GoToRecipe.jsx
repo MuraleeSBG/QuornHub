487e9595f83176c8f32d7f70f7a8501f6c3a4f03
JenniferFernandes-SBG <125460157+JFernandes-dev@users.noreply.github.com>
2023-11-03 10:28:10 +0000

    updating css for go to recipe

--- frontend/src/pages/GoToRecipe/GoToRecipe.jsx
+++ frontend/src/pages/GoToRecipe/GoToRecipe.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer"
 import React, { useState, useEffect } from 'react';
+import './GoToRecipe.scss';
 
 const GoToRecipe = () => {
     const [data, setData] = useState([]);
@@ -22,8 +23,7 @@ const GoToRecipe = () => {
         })
         .then(data => {
             setData(data);
-            console.log(data[0].recipeName)
-            
+            console.log(data[0])   
 
         })
         .catch(error => {
@@ -33,6 +33,18 @@ const GoToRecipe = () => {
     }, [id])
 
 
+    // function listIngredients() {
+    //     const ingredients = (JSON.parse(data[0].ingredients))
+    //     ingredients.map((ingredient) => {
+    //         console.log(ingredient)
+    //         return <p>{ingredient}</p>
+    //     })
+    //     return <p>{ingredient}</p>
+
+    // }
+
+
+    
 	return (
 		<div className="recipe-main">
             <Header />
@@ -40,15 +52,35 @@ const GoToRecipe = () => {
             <div className="recipe-top">
                     {data.length !== 0 ? (
 						<div>
-                            <div>{data[0].recipeName}</div>
-                            
+                            <div className="overview">
+                                <div className="overview-col1">
+                                    <img className="rec-image" src={`/recipeImages/${data[0].recipeImg}`} alt={data[0].recipeName}/>
+                                </div>
+                                <div className="overview-col2">
+                                    <div className="recipe-title">{data[0].recipeName}</div>
+                                    <div className="tags"></div>
+                                </div> 
+
+                            </div>
+                            <div className="main">
+                                <div className="main-column1">
+                                    <h4 className="sub-heading">Ingredients</h4>
+                                    <div className="ingredients-list">{(JSON.parse(data[0].ingredients)).map((ingredient) => {return (<p className="ingredient">{ingredient}</p>)})}</div>
+                                </div>
+                                <div className="main-column2">
+                                    <h4 className="sub-heading">Method</h4>
+                                    <div className="method"> {(data[0].method)}</div>
+                                </div> 
+                                 
+                            </div>
+
                             
 
                         </div>
 					) : (
 						<div>
 							<h1 className="results-placeholder">
-								error 404
+								error 404 - page not found
 							</h1>
 						</div>
 					)}
