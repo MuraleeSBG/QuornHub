8ba06af764c98081d016126f627cf249a9fa9ab9
JenniferFernandes-SBG <jennifer.fernandes@flutteruki.com>
2023-11-06 14:58:30 +0000

    responsive alignments on food saver card - button at bottom right always

--- frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
+++ frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
@@ -3,16 +3,16 @@ import "./FoodSaverCard.scss";
 export const FoodSaverCard = ({ selectedRecipe, tags, onRecipeSelect }) => {
 	const url = `/go-to-recipe/${selectedRecipe.id}`
 	return (
-		<div className="card">
+		<div className="food-saver-card">
 			<img className="recipe-image" src={`/recipeImages/${selectedRecipe.recipeImg}`} alt={selectedRecipe.recipeName} />
-			<div className="card-body">
-				<h3 className="card-title">{selectedRecipe.recipeName}</h3>
-				<div className="tag-container">
+			<div className="fs-card-body">
+				<h3 className="fs-card-title">{selectedRecipe.recipeName}</h3>
+				<div className="fs-tag-container">
 					{tags.map((tag, index) => (
-						<p key={index} className="tag-text">{tag}</p>
+						<p key={index} className="fs-tag-text">{tag}</p>
 					))}
 				</div>
-				<div className="button-container">
+				<div className="recipe-button-container">
 					<button className="recipe-button"><a href={url}>Go to recipe </a></button>
 				</div>
 			</div>
