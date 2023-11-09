625c8e38a57a17dbe9a479a5a654ae6d590b1a27
Alice Smith <alice.smith@skybettingandgaming.com>
2023-11-09 22:55:58 +0000

    displaying images on cards

--- frontend/src/components/CategoryCard/CategoryCard.jsx
+++ frontend/src/components/CategoryCard/CategoryCard.jsx
@@ -1,17 +1,24 @@
 import "./CategoryCard.scss";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 // Could not just use preview card as it forms the url differently
 
 export const CategoryCard = ({ category }) => {
-	const url = `/${category.link}`
+	const url = `/${category.link}`;
 	return (
 		<div className="category-card">
-            <Link className="link" to={url}>
-                <img className="category-image" src={`/recipeImages/${category.recipeImg}`} alt={category.recipeName} />
-            </Link>
-            <p className="category-container"><a href={url} className="category-subtitle">{category.recipeName}</a></p>
-
+			<Link className="link" to={url}>
+				<img
+					className="category-image"
+					src={`/recipeImages/${category.recipeImg}`}
+					alt={category.recipeName}
+				/>
+			</Link>
+			<p className="category-container">
+				<a href={url} className="category-subtitle">
+					{category.recipeName}
+				</a>
+			</p>
 		</div>
 	);
 };
--- frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
+++ frontend/src/components/FoodSaverCard/FoodSaverCard.jsx
@@ -1,19 +1,27 @@
 import "./FoodSaverCard.scss";
 
 export const FoodSaverCard = ({ selectedRecipe, tags, onRecipeSelect }) => {
-	const url = `/go-to-recipe/${selectedRecipe.id}`
+	const url = `/go-to-recipe/${selectedRecipe.id}`;
 	return (
 		<div className="card">
-			<img className="recipe-image" src={`/recipeImages/${selectedRecipe.recipeImg}`} alt={selectedRecipe.recipeName} />
+			<img
+				className="recipe-image"
+				src={`http://localhost:3001/uploads/${selectedRecipe.recipeImg}`}
+				alt={selectedRecipe.recipeName}
+			/>
 			<div className="card-body">
 				<h3 className="card-title">{selectedRecipe.recipeName}</h3>
 				<div className="tag-container">
 					{tags.map((tag, index) => (
-						<p key={index} className="tag-text">{tag}</p>
+						<p key={index} className="tag-text">
+							{tag}
+						</p>
 					))}
 				</div>
 				<div className="button-container">
-					<button className="recipe-button"><a href={url}>Go to recipe </a></button>
+					<button className="recipe-button">
+						<a href={url}>Go to recipe </a>
+					</button>
 				</div>
 			</div>
 		</div>
