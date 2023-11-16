bea767d4caee364ae62802b6094c86cc2db5ee2c
JenniferFernandes-SBG <jennifer.fernandes@flutteruki.com>
2023-11-16 13:48:07 +0000

    updating to main's file directory set up

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -1,7 +1,7 @@
-import { Header } from '../../components/Header/Header'
-import { CategoryCard } from "../../components/CategoryCard/CategoryCard";
-import './Recipes.scss'
-import {Footer} from '../../components/Footer/Footer'
+import { Header } from '../components/Header/Header'
+import { CategoryCard } from "../components/CategoryCard/CategoryCard";
+import '.Categories';
+import {Footer} from '../components/Footer/Footer'
 
 const AllRecipes = () => {
 
