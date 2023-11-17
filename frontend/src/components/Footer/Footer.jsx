7a2077dfcfecf1bfbe6579a883a44bdfbd55a8d7
Alice Smith <alice.smith@skybettingandgaming.com>
2023-11-17 14:19:53 +0000

    fixing the main homepage

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 	return (
 		<div className="footer">
 			<h1 className="footer-title">Daily deliciousness in your inbox</h1>
-			<p className="sub-heading">Enjoy weekly hand picked recipes and food waste tips</p>
+			<p className="sub-heading-footer">Enjoy weekly hand picked recipes and food waste tips</p>
 			<div className="joinEmail">
 				<input className="join-input" type="text" placeholder="Email Address..." />
 				<button className="join-button">Join</button>
