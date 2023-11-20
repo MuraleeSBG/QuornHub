bde4f0484ac63d67a92a5deb32f53c7ea114b718
Alice Smith <alicesmith@Alices-MacBook-Pro.local>
2023-11-20 18:12:06 +0000

    cleaning up code

--- frontend/src/pages/FoodSaver/FoodSaver.jsx
+++ frontend/src/pages/FoodSaver/FoodSaver.jsx
@@ -3,156 +3,154 @@ import { FoodSaverHeader } from "../../components/FoodSaverHeader/FoodSaverHeade
 import { useState, useEffect } from "react";
 import { FoodSaverCard } from "../../components/FoodSaverCard/FoodSaverCard";
 import "./FoodSaver.scss";
-import close from "../../images/close.svg"
+import close from "../../images/close.svg";
 
 const FoodSaver = () => {
-	const [listOfInput, setListOfInput] = useState([]);
-	const [recipesToDisplay, setrecipesToDisplay] = useState([]);
-	const [inputText, setInputText] = useState("");
+  const [ingredientSearchTerms, setIngredientSearchTerms] = useState([]);
+  const [recipesToDisplay, setRecipesToDisplay] = useState([]);
+  const [inputText, setInputText] = useState("");
 
-	// This needs replacing with code commented out below to fetch from database
-	// const data = require("../../testData.json");
+  const [data, setData] = useState([]);
 
-	const [data, setData] = useState([]);
+  useEffect(() => {
+    const apiUrl = `http://localhost:3001/recipes`;
 
-	useEffect(() => {
-		const apiUrl = `http://localhost:3001/recipes`;
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error with response");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-		fetch(apiUrl)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Error with response");
-				}
-				return response.json();
-			})
-			.then((data) => {
-				setData(data);
-			})
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-			});
-	}, []);
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addIngredient();
+    }
+  };
 
-	const handleKeyDown = (e) => {
-		if (e.key === "Enter") {
-			addIngredient();
-		}
-	};
+  // sets input into state, clears input box and calls filterData to implement search
+  const addIngredient = () => {
+    if (
+      ingredientSearchTerms.some(
+        (item) => item.toLowerCase() === inputText.toLowerCase()
+      ) ||
+      inputText === ""
+    ) {
+      return;
+    }
+    setIngredientSearchTerms((currentList) => [...currentList, inputText]);
+    setInputText("");
+  };
 
-	// sets input into state, clears input box and calls filterData to implement search
-	const addIngredient = () => {
-		if (
-			listOfInput.some(
-				(item) => item.toLowerCase() === inputText.toLowerCase()
-			) ||
-			inputText === ""
-		) {
-			return;
-		}
-		setListOfInput((currentList) => [...currentList, inputText]);
-		setInputText("");
-	};
+  // searches for recipes based on new input
+  useEffect(() => {
+    const filteredRecipes = data.filter((recipe) => {
+      return recipe.ingredients.some((ingredient) => {
+        const ingredientStr =
+          typeof ingredient === "string" ? ingredient : ingredient.ingredient;
 
-	// searches for recipes based on new input
-	useEffect(() => {
-		const filteredRecipes = data.filter((recipe) => {
-			return recipe.ingredients.some((ingredient) => {
-				const finalIngredient =
-					typeof ingredient === "string" ? ingredient : ingredient.ingredient;
-				return listOfInput.some(
-					(input) => input.toLowerCase() === finalIngredient.toLowerCase()
-				);
-			});
-		});
-		setrecipesToDisplay(filteredRecipes);
-	}, [listOfInput, data]);
+        return ingredientSearchTerms.some(
+          (ingredientSearchTerm) =>
+            ingredientSearchTerm.toLowerCase() === ingredientStr.toLowerCase()
+        );
+      });
+    });
+    setRecipesToDisplay(filteredRecipes);
+  }, [ingredientSearchTerms, data]);
 
-	const removeIngredient = (e) => {
-		const ingredientToRemove = e.target.parentElement.id;
-		const deleteByvalue = (ingredientToRemove) => {
-			setListOfInput((oldList) => {
-				return oldList.filter((item) => item !== ingredientToRemove);
-			});
-		};
-		deleteByvalue(ingredientToRemove);
-	};
+  const removeIngredient = (e) => {
+    const ingredientToRemove = e.target.parentElement.id;
+    const newIngredientSearchTerms = ingredientSearchTerms.filter(
+      (item) => item !== ingredientToRemove
+    );
+    setIngredientSearchTerms(newIngredientSearchTerms);
+  };
 
-	// input tags NOT dietaries
-	const tags = listOfInput.map((item, index) => {
-		return (
-			<div className="tag" id={item} key={index}>
-				{item}
-				<button
-					id={`${item}btn`}
-					onClick={removeIngredient}
-					className="remove-button"
-				>
-					<img className="remove-button-icon" src={close} alt="remove"/>
-				</button>
-			</div>
-		);
-	});
+  // input tags NOT dietaries
+  const tags = ingredientSearchTerms.map((item, index) => {
+    return (
+      <div className="tag" id={item} key={index}>
+        {item}
+        <button
+          id={`${item}`}
+          onClick={removeIngredient}
+          className="remove-button"
+        >
+          <img className="remove-button-icon" src={close} alt="remove" />
+        </button>
+      </div>
+    );
+  });
 
-	// dietary tags
-	const getTags = (recipe) => {
-		const tags = [];
-		if (recipe.isLactoseFree) {
-			tags.push("Lactose Free");
-		}
-		if (recipe.isVegan) {
-			tags.push("Vegan");
-		}
-		if (recipe.isGlutenFree) {
-			tags.push("Gluten Free");
-		}
-		if (recipe.isDairyFree) {
-			tags.push("Dairy Free");
-		}
-		if (recipe.isNutFree) {
-			tags.push("Nut Free");
-		}
-		return tags;
-	};
-	const showResults = recipesToDisplay.map((recipe, index) => {
-		const tags = getTags(recipe);
-		return <FoodSaverCard key={index} selectedRecipe={recipe} tags={tags} />;
-	});
+  // dietary tags
+  const getTags = (recipe) => {
+    const tags = [];
+    if (recipe.isLactoseFree) {
+      tags.push("Lactose Free");
+    }
+    if (recipe.isVegan) {
+      tags.push("Vegan");
+    }
+    if (recipe.isGlutenFree) {
+      tags.push("Gluten Free");
+    }
+    if (recipe.isDairyFree) {
+      tags.push("Dairy Free");
+    }
+    if (recipe.isNutFree) {
+      tags.push("Nut Free");
+    }
+    return tags;
+  };
 
-	return (
-		<div className="food-saver-page">
-			<FoodSaverHeader />
-			<div className="food-saver-main">
-				<div className="search-bar">
-					<input
-						className="search-input"
-						type="text"
-						placeholder="Type in an ingredient..."
-						value={inputText}
-						onChange={(e) => setInputText(e.target.value)}
-						onKeyDown={handleKeyDown}
-					/>
-					<button className="submit-button" onClick={addIngredient}>
-						Search
-					</button>
-				</div>
-				<div id="tags" className="search-tags-container">
-					{tags}
-				</div>
+  const showResults = recipesToDisplay.map((recipe, index) => {
+    const tags = getTags(recipe);
+    return <FoodSaverCard key={index} selectedRecipe={recipe} tags={tags} />;
+  });
 
-				{listOfInput.length !== 0 ? (
-					<div className="results-container">{showResults}</div>
-				) : (
-					<div>
-						<h1 className="results-placeholder">
-							Enter ingredients to start saving food and be inspired!
-						</h1>
-					</div>
-				)}
-			</div>
+  return (
+    <div className="food-saver-page">
+      <FoodSaverHeader />
+      <div className="food-saver-main">
+        <div className="search-bar">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Type in an ingredient..."
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="submit-button" onClick={addIngredient}>
+            Search
+          </button>
+        </div>
+        <div id="tags" className="search-tags-container">
+          {tags}
+        </div>
 
-			<Footer />
-		</div>
-	);
+        {ingredientSearchTerms.length !== 0 ? (
+          <div className="results-container">{showResults}</div>
+        ) : (
+          <div>
+            <h1 className="results-placeholder">
+              Enter ingredients to start saving food and be inspired!
+            </h1>
+          </div>
+        )}
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default FoodSaver;
