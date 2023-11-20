040386e29f4f972ffb9ca8945186dab49f21c03c
Alice Smith <alice.smith@skybettingandgaming.com>
2023-11-20 20:59:31 +0000

    WIP - started mobile menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,74 +1,92 @@
 import "./Header.scss";
 import logo from "../../images/QHLogo.png";
 import user from "../../images/user.svg";
+import menu from "../../images/menu.svg";
+import close from "../../images/close.svg";
 
 import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { isLoggedIn, logout } from "../../utils/authUtils";
+import { useState } from "react";
 
 export const Header = ({ userName }) => {
-  const dropIcon = <FontAwesomeIcon icon={faCaretDown} />;
-  const navigate = useNavigate();
+	const dropIcon = <FontAwesomeIcon icon={faCaretDown} />;
+	const navigate = useNavigate();
+	const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-  const onLogout = () => {
-    const userApiUrl = `http://localhost:3001/logout`;
-    fetch(userApiUrl, {
-      credentials: "include",
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Unable to logout");
-        }
-        logout();
-        navigate("/");
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  };
+	const onLogout = () => {
+		const userApiUrl = `http://localhost:3001/logout`;
+		fetch(userApiUrl, {
+			credentials: "include",
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Unable to logout");
+				}
+				logout();
+				navigate("/");
+			})
+			.catch((error) => {
+				console.error(error.message);
+			});
+	};
 
-  return (
-    <div className="header">
-      <div className="nav-bar">
-        <div>
-          <Link className="nav" to="/">
-            <img className="QHlogo-header" src={logo} alt="Quornhub logo" />
-          </Link>
-        </div>
+	return (
+		<>
+			<div className="nav-bar">
+				<button
+					className="nav-mobile-menu header-button"
+					onClick={() => setShowMobileMenu(!showMobileMenu)}
+				>
+					<img className="header-icon" src={menu} alt="menu"></img>
+				</button>
+				<div>
+					<Link className="nav" to="/">
+						<img className="QHlogo-header" src={logo} alt="Quornhub logo" />
+					</Link>
+				</div>
 
-        <div className="nav-links">
-          <Link className="nav" to="/">
-            Home
-          </Link>
-          <Link className="nav" to="/food-saver">
-            Food Saver
-          </Link>
-          <div className="dropdown nav">
-            Recipes {dropIcon}
-            <div className="dropdown-content2">
-              <Link to="/gluten-free">Gluten Free</Link>
-              <Link to="/vegan">Vegan</Link>
-              <Link to="/fifteen-minute">15 Minute</Link>
-              <Link to="/lactose-free">Lactose Free</Link>
-              <Link to="/nut-free">Nut Free</Link>
-              {isLoggedIn() && <Link to="/add-recipes">Add a Recipe</Link>}
-              <Link to="/recipes">All Recipes</Link>
-            </div>
-          </div>
-        </div>
+				<div className="nav-links">
+					<Link className="nav" to="/">
+						Home
+					</Link>
+					<Link className="nav" to="/food-saver">
+						Food Saver
+					</Link>
+					<div className="dropdown nav">
+						Recipes {dropIcon}
+						<div className="dropdown-content2">
+							<Link to="/gluten-free">Gluten Free</Link>
+							<Link to="/vegan">Vegan</Link>
+							<Link to="/fifteen-minute">15 Minute</Link>
+							<Link to="/lactose-free">Lactose Free</Link>
+							<Link to="/nut-free">Nut Free</Link>
+							{isLoggedIn() && <Link to="/add-recipes">Add a Recipe</Link>}
+							<Link to="/recipes">All Recipes</Link>
+						</div>
+					</div>
+				</div>
 
-        <div className="dropdown">
-          <img className="user-icon" src={user} alt="login" />
-          <div className="dropdown-content">
-            {isLoggedIn() ? (
-              <Link onClick={onLogout}>Logout</Link>
-            ) : (
-              <Link to="/login">Login / Register</Link>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="dropdown">
+					<img className="user-icon header-icon" src={user} alt="login" />
+					<div className="dropdown-content">
+						{isLoggedIn() ? (
+							<Link onClick={onLogout}>Logout</Link>
+						) : (
+							<Link to="/login">Login / Register</Link>
+						)}
+					</div>
+				</div>
+			</div>
+			{showMobileMenu && (
+				<div className="mobile-menu">
+					<div className="mobile-menu-items"></div>
+					<button className = "close-button" onClick={() => setShowMobileMenu(!showMobileMenu)}>
+						<img className="header-icon close-icon" src={close}></img>
+					</button>
+				</div>
+			)}
+		</>
+	);
 };
